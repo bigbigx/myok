8f867a8f3aa2667c58d340c6008109e448e34018
Lenovo <jianglibin@zskuaixiao.com>
2018-03-02 13:39:16 +0800

    second commit11

--- webpage/src/components/Order/SQLsyntax.vue
+++ webpage/src/components/Order/SQLsyntax.vue
@@ -206,6 +206,8 @@ export default {
           'data_ddl_dml': this.formItem.textarea_ddl_dml
         })
         .then(res => {
+          alert(res.data['select'])
+          alert(res.data['dml_ddl'])
           this.formItem.textarea_backup = res.data['select']
           this.formItem.textarea_ddl_dml = res.data['dml_ddl']
         })
