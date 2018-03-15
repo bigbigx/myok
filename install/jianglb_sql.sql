19a076a4dcb02a825fd19c9d1a3b8548f6221a8a
Lenovo <jianglibin@zskuaixiao.com>
2018-03-15 17:54:58 +0800

    second commit11

--- install/jianglb_sql.sql
+++ install/jianglb_sql.sql
@@ -1,3 +1,16 @@
 
 
+1、安装sqlite3，创建token_db_new数据库
+sudo apt-get install sqlite3
+sqlite3 -version
 
+cd /root/
+mkdir
+sqlite3 token.db
+
+2、 创建 token_db_new 数据表
+create table token_db_new(
+username char(20) not null,
+workid  char(50) not null,
+token char(50) not null
+)
