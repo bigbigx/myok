d8cab8dd5d5cfe295cd21963c68387345e961272
Lenovo <jianglibin@zskuaixiao.com>
2018-03-30 15:00:15 +0800

    second commit11

--- src/settingConf/settings.py
+++ src/settingConf/settings.py
@@ -109,7 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+
+LANGUAGE_CODE = 'utf-8'
 
 TIME_ZONE = 'Asia/Shanghai'
 
